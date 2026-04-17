3abe850604f5ffa49bed5cec966f00e720374aa1
sinan722007-cell <sinan722007@gmail.com>
2026-04-17 21:08:13 +0000

    fix: reorganize dependencies, add ESLint and Jest configs, improve CI/CD workflows and Procfile

---
+++ backend/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts'
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/'
+  ],
+  setupFilesAfterEnv: [],
+  testTimeout: 10000
+};
